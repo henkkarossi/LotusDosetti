63f26b3ed353605d45c7813ddb8983b26f64585d
melliswee <55910298+melliswee@users.noreply.github.com>
2020-04-30 12:14:40 +0300

    korjattu tiedostorakenne eclipsesta

--- Dosetti/src/toiminnallisuus/Main.java
+++ Dosetti/src/toiminnallisuus/Main.java
@@ -8,13 +8,14 @@ public class Main {
 	
 	public static void main(String[] args) throws ParseException {
 		
+		
 		pl = new PillDispenser();
 
 		DataManager.CreateNewBaseData();
 		//DataManager.SaveData();
 		DataManager.LoadData();
 		
-		System.out.print(pl.slots[4].getState());
+		System.out.print(pl.slots[0].getState());
 				
 		PillDispenser.running = true;
 	}
